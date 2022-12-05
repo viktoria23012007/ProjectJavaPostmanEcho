864497c57ff2132652c6e293f03af49439ab5623
Виктория Вареник <viktoria.varenik@yandex.ru>
2022-12-05 18:35:40 +0300

    Добавлены тесты

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,28 @@
+package ru.netolofy.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnSomeData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Тест") // отправляемые данные
+                .header("header", "value")
+                .queryParam("param", "value")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Тест"))
+        ;
+    }
+}
